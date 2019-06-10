43aa28bd0a47d29b785f535a710bbe97553fc6e7
Matt Fasano <matt.fasano88@gmail.com>
2019-06-10 10:46:01 -0400

    update js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-ScrollReveal().reveal('.video', { reset: true, delay: 200, easing: 'step(5)' });
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    _copyToClipBoard = () => {
+        let copyText = document.getElementById("email");
+
+        copyText.select();
+        document.execCommand("copy");
+
+        alert(`Copied ${copyText.value} to clipboard!`);
+    }
+
+    ScrollReveal().reveal('.video', { reset: true, delay: 200, easing: 'step(5)' });
+});
